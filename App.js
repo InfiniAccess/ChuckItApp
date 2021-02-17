d73a39890ec2f0cdeb3e40319d357ecfee9504ee
Sebastian Schreuder <sgschreuder@gmail.com>
2021-02-17 02:19:12 -0600

    Added Image Input Component. Need to debug minor issue with rendering. Added request permissions for Media Library Access.

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
+import Screen from "./app/components/Screen";
+
+
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import AccountScreen from './app/screens/AccountScreen';
 import ListingsScreen from './app/screens/ListingsScreen';
@@ -10,9 +14,26 @@ import ViewImageScreen from './app/screens/ViewImageScreen';
 import LoginScreen from './app/screens/LoginScreen';
 import RegisterScreen from './app/screens/RegisterScreen';
 import ListingEditScreen from './app/screens/ListingEditScreen';
+import { Button, Image } from 'react-native';
+import ImageInput from './app/components/ImageInput';
 
 export default function App() {
+  const [imageUri, setImageUri] = useState();
+
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (!result.granted) alert('You need to enable permission to access the library.');
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, [])
+  
+
   return (
-    <ListingEditScreen></ListingEditScreen>
+    <Screen>
+      <ImageInput imageUri={imageUri}
+      onChangeImage={uri => setImageUri(uri)}/>
+    </Screen>
   );
 }
